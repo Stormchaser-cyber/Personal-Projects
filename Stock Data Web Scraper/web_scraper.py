75920d254516c0c963e1edb1197fa91202600624
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-15 09:51:45 -0500

    Commented out file

--- Stock Data Web Scraper/web_scraper.py
+++ Stock Data Web Scraper/web_scraper.py
@@ -5,20 +5,23 @@
 
 # URL: https://www.google.com/finance/
 
-import requests
-from bs4 import BeautifulSoup
-import sys
-
-URL = "https://www.nasdaq.com/market-activity/stocks/screener"
-response = requests.get(URL)
-
-#print(response.content)
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-results = soup.find(id="main-content") # div.sbnBtf
-
-if (results != None):
-    print(results.prettify())
-else:
-    print("Results didn't have any values")
+##import requests
+##from bs4 import BeautifulSoup
+##import sys
+##
+##URL = "https://www.nasdaq.com/market-activity/stocks/screener"
+##response = requests.get(URL)
+##
+###print(response.content)
+##
+##soup = BeautifulSoup(response.content, "html.parser")
+##
+##results = soup.find(id="nasdaq-screener__form-button") # main-content
+##
+##if (results != None):
+##    print(results.prettify())
+##else:
+##    print("Results didn't have any values")
+##
+##
+###/html/body/div[2]/div/main/div[2]/article/div[3]/div[1]/div/div/div[3]/div[2]/div[2]/div/button
