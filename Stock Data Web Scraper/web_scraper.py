d5b7134fa25fd02f3c47f9b05c313778d21b189c
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-14 20:43:50 -0500

    Attempted to fix errors in code, still not working

--- Stock Data Web Scraper/web_scraper.py
+++ Stock Data Web Scraper/web_scraper.py
@@ -3,3 +3,22 @@
 # July 2021 -- Created -- Ted Strombeck
 #
 
+# URL: https://www.google.com/finance/
+
+import requests
+from bs4 import BeautifulSoup
+import sys
+
+URL = "https://www.nasdaq.com/market-activity/stocks/screener"
+response = requests.get(URL)
+
+#print(response.content)
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+results = soup.find(id="main-content") # div.sbnBtf
+
+if (results != None):
+    print(results.prettify())
+else:
+    print("Results didn't have any values")
