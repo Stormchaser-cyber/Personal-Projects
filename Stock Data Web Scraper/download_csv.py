e182af9ec4f0e209302ca9fca5c78803336841d9
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-15 09:20:29 -0500

    Got initial code to compile!

--- Stock Data Web Scraper/download_csv.py
+++ Stock Data Web Scraper/download_csv.py
@@ -4,11 +4,15 @@
 # ID: "nasdaq-screener__form-button--download ns-download-1")
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-browser = webdriver.Chrome('C:\ProgramData\Microsoft\Windows\Start Menu\Programs')
+browser = webdriver.Chrome()
 
 browser.get("https://www.nasdaq.com/market-activity/stocks/screener")
 
-button = browser.find_element_by_css_selector('/html/body/div[2]/div/main/div[2]/article/div[3]/div[1]/div/div/div[3]/div[2]/div[2]/div/button')
+element = browser.find_element_by_xpath('/html/body/div[2]/div/main/div[2]/article/div[3]/div[1]/div/div/div[3]/div[2]/div[2]/div/button')
+#button = browser.find_element_by_id('nasdaq-screener__form-button--download ns-download-1')
+print(element.get_attribute('class'))
+browser.close()
 
 # Doesn't work -- Not really sure why
