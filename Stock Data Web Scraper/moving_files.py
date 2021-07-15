cc8b5094b5ecb3949f6579e688957268bbb1db7c
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-15 16:47:18 -0500

    Added documentation for the move_file function

--- Stock Data Web Scraper/moving_files.py
+++ Stock Data Web Scraper/moving_files.py
@@ -5,6 +5,18 @@
 import shutil
 
 def move_file(file_name, file_type, source_folder, destination_folder):
+    """
+    move_file function moves a given file where a file's name, type, source folder, and destination folder are specified
+
+    Parameters
+    ----------
+    String
+        file_name: the name of the file
+        file_type: the suffix of the file which indicates what type of file it is
+        source_folder: the file path of the source folder (NOTE: it should end without a slash or backslash)
+        destination_folder: the file path of the desired folder (NOTE: it should end without a slash or backslash)
+    """
+    
     source_string = source_folder+ '/' + file_name + file_type # Adding the extra backslash so that it goes into the directory instead of looking above it
     destination_string = destination_folder + '/' + file_name + file_type
 
