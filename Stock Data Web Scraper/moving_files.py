7bc132670d28462d257ba879146c340e5f3ce520
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-15 18:28:29 -0500

    Created scan_folder function with initial code

--- Stock Data Web Scraper/moving_files.py
+++ Stock Data Web Scraper/moving_files.py
@@ -3,22 +3,32 @@
 # July 2021 -- Created -- Ted Strombeck
 #
 import shutil
+from os import listdir
+from os.path import isfile
 
-def move_file(file_name, file_type, source_folder, destination_folder):
+def scan_folder(source_folder):
+    files_to_move = []
+    
+    for item in listdir(source_folder):
+        if item[0:15] == 'nasdaq_screener':
+            files_to_move.append(item)
+
+    return files_to_move
+
+def move_file(file_name, source_folder, destination_folder):
     """
     move_file function moves a given file where a file's name, type, source folder, and destination folder are specified
 
     Parameters
     ----------
     String
-        file_name: the name of the file
-        file_type: the suffix of the file which indicates what type of file it is
+        file_name: the full name of the file
         source_folder: the file path of the source folder (NOTE: it should end without a slash or backslash)
         destination_folder: the file path of the desired folder (NOTE: it should end without a slash or backslash)
     """
     
-    source_string = source_folder+ '/' + file_name + file_type # Adding the extra backslash so that it goes into the directory instead of looking above it
-    destination_string = destination_folder + '/' + file_name + file_type
+    source_string = source_folder+ '/' + file_name # Adding the extra backslash so that it goes into the directory instead of looking above it
+    destination_string = destination_folder + '/' + file_name
 
     shutil.move(source_string, destination_string) # actually moving the file
 
@@ -26,12 +36,16 @@ def main():
     """
     Main function to run test code and to get initial code working
     """
-    file_type = '.csv'
-    file_name = 'nasdaq_screener_1626359675325'
+    file_name = 'nasdaq_screener_1626390668384.csv'
     source_folder = 'C:/Users/tedst/Downloads'
     destination_folder = 'C:/Users/tedst/Documents/Augsburg University Files/Programming Files/Bot Projects/Stock Data Web Scraper/Stock Spreadsheets'
 
-    move_file(file_name=file_name, file_type=file_type, source_folder=source_folder, destination_folder=destination_folder)
+    #move_file(file_name=file_name, file_type=file_type, source_folder=source_folder, destination_folder=destination_folder)
 
+    items_to_move = scan_folder(source_folder)
+
+    for item in items_to_move:
+        move_file(item, source_folder, destination_folder)
+    
 if __name__ == "__main__":
     main()
