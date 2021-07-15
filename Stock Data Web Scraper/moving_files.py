85c4d150e30336cc536e37910b4d9ff1529fac5c
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-15 16:44:50 -0500

    Created move_file function with working code

--- Stock Data Web Scraper/moving_files.py
+++ Stock Data Web Scraper/moving_files.py
@@ -2,10 +2,24 @@
 #
 # July 2021 -- Created -- Ted Strombeck
 #
+import shutil
 
+def move_file(file_name, file_type, source_folder, destination_folder):
+    source_string = source_folder+ '/' + file_name + file_type # Adding the extra backslash so that it goes into the directory instead of looking above it
+    destination_string = destination_folder + '/' + file_name + file_type
+
+    shutil.move(source_string, destination_string) # actually moving the file
 
 def main():
-    
+    """
+    Main function to run test code and to get initial code working
+    """
+    file_type = '.csv'
+    file_name = 'nasdaq_screener_1626359675325'
+    source_folder = 'C:/Users/tedst/Downloads'
+    destination_folder = 'C:/Users/tedst/Documents/Augsburg University Files/Programming Files/Bot Projects/Stock Data Web Scraper/Stock Spreadsheets'
+
+    move_file(file_name=file_name, file_type=file_type, source_folder=source_folder, destination_folder=destination_folder)
 
 if __name__ == "__main__":
     main()
