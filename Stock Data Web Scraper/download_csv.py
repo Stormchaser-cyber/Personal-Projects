fb3ef77f79b060ebd3f4bef7d5698b10a5bef980
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-15 09:45:07 -0500

    Added download_csv_from_url_by_xpath function and implemented it in main function

--- Stock Data Web Scraper/download_csv.py
+++ Stock Data Web Scraper/download_csv.py
@@ -5,21 +5,25 @@
 
 from selenium import webdriver
 import time
-from selenium.webdriver.common.keys import Keys
+
+def download_csv_from_url_by_xpath(url, xpath):
+    browser = webdriver.Chrome()
+
+    browser.get(url)
+
+    browser.find_element_by_xpath(xpath).click()
+
+    time.sleep(5)
+
+    browser.quit()
 
 def main():
     """
     Main function to control and run the functions as you initially would in main
     """
-    browser = webdriver.Chrome()
 
-    browser.get("https://www.nasdaq.com/market-activity/stocks/screener")
-
-    element = browser.find_element_by_xpath('/html/body/div[2]/div/main/div[2]/article/div[3]/div[1]/div/div/div[3]/div[2]/div[2]/div/button').click()
-    
-    time.sleep(5)
-
-    browser.quit()
+    download_csv_from_url_by_xpath(url="https://www.nasdaq.com/market-activity/stocks/screener",
+                                   xpath='/html/body/div[2]/div/main/div[2]/article/div[3]/div[1]/div/div/div[3]/div[2]/div[2]/div/button')
 
 if __name__ == "__main__":
     main()
