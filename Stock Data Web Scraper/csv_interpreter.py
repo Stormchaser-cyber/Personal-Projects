5c8000326a900b3ba173497ad37b8bbcec7faa3f
StormchaserTed <ted.strombeck1@gmail.com>
2021-07-16 10:01:29 -0500

    Added update_sorted_spreadsheet_records function with code and doucmentation

--- Stock Data Web Scraper/csv_interpreter.py
+++ Stock Data Web Scraper/csv_interpreter.py
@@ -5,9 +5,36 @@
 
 from os import listdir
 from os.path import isfile
+import csv
+
+def update_sorted_spreadsheet_records(filename):
+    """
+    update_sorted_spreadsheet_records function updates the current file
+    records to add a filename to the end of the csv file
+
+    Parameters
+    ----------
+    String
+        filename: the string name of the file to be added to the csv file
+    """
+    with open('sorted_spreadsheet_records.csv', mode='a', newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow([filename])
 
 def main():
+    files_already_sorted = []
     
+    with open('sorted_spreadsheet_records') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count != 0:
+                files_already_sorted.append(row)
+            line_count += 1
+
+    print(files_already_sorted)
+        
+        
 
 if __name__ == '__main__':
-    main()
+    #main()
