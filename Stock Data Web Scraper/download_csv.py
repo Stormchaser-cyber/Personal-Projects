33cc951a849e2dd20b320d4ed66e1e9ede2773c4
StormchaserTed <ted.strombeck1@gmail.com>
2021-08-25 17:23:03 -0500

    Got webscraper working again and added code to surpress warnings if they are not fatal

--- Stock Data Web Scraper/download_csv.py
+++ Stock Data Web Scraper/download_csv.py
@@ -4,6 +4,7 @@
 # ID: "nasdaq-screener__form-button--download ns-download-1")
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 
 def download_csv_from_url_by_xpath(url, xpath):
@@ -17,11 +18,22 @@ def download_csv_from_url_by_xpath(url, xpath):
         url: the string of the url of the website
         xpath: the xpath of the element in the html code
     """
-    browser = webdriver.Chrome() # Creating a chrome browser object
+
+    ### Works! Just takes a little bit of awhile, not too sure why ###
+    options = webdriver.ChromeOptions()
+    
+    #options.add_argument('--headless') #Not showing the webpage pop up
+    
+    options.add_argument('--log-level=3') #Only showing logs if they are fatal
+    
+    browser = webdriver.Chrome(options=options) # Creating a chrome browser object
 
     browser.get(url) # looking up the URL of the website
 
-    browser.find_element_by_xpath(xpath).click() # navigating through the xpath and clicking on the element
+    try:
+        browser.find_element_by_xpath(xpath).click() # navigating through the xpath and clicking on the element
+    except:
+        print('Didn\'t work')
 
     time.sleep(5) # sleeping for 5 seconds to ensure that the download is able to go through
 
@@ -32,8 +44,13 @@ def main():
     Main function to control and run the functions as you initially would in main
     """
 
+    
+
     download_csv_from_url_by_xpath(url="https://www.nasdaq.com/market-activity/stocks/screener",
                                    xpath='/html/body/div[2]/div/main/div[2]/article/div[3]/div[1]/div/div/div[3]/div[2]/div[2]/div/button')
+    
+    
+
 
 if __name__ == "__main__":
     main()
