af0ac1d2a0ecd8177537f68feedeebbc30ca2da0
StormchaserTed <ted.strombeck1@gmail.com>
2021-08-26 09:40:45 -0500

    Removed extra comments and code

--- Stock Data Web Scraper/csv_interpreter.py
+++ Stock Data Web Scraper/csv_interpreter.py
@@ -34,30 +34,6 @@ def update_sorted_spreadsheet_records(filename):
         writer = csv.writer(csv_file, delimiter=',')
         writer.writerow([filename + '.csv'])
 
-def get_files_already_sorted():
-    """
-    get_files_already_sorted function scans the records spreadsheet and
-    returns a list of all of the file names already sorted
-
-    Returns
-    -------
-    List
-        files_already_sorted: the list containing the file names as strings that
-                              are already sorted.
-    """
-    files_already_sorted = []
-    
-    with open('sorted_spreadsheet_records.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for item in csv_reader:
-            if line_count != 0:
-                files_already_sorted.append("".join(item))
-                
-            line_count += 1
-
-    return files_already_sorted
-
 def clean_file(filename):
 
     with open('C:/Users/tedst/Documents/Augsburg University Files/Programming Files/Bot Projects/Stock Data Web Scraper/Stock Spreadsheets/' + filename,'r') as input_file:
@@ -111,21 +87,17 @@ def rename_file(file_name):
     
 
 def main():
-    sorted_files = get_files_already_sorted()
     criteria = 'nasdaq_screener'
     source_folder_file_path = 'C:/Users/tedst/Documents/Augsburg University Files/Programming Files/Bot Projects/Stock Data Web Scraper/Stock Spreadsheets'
     record_files = moving_files.scan_folder(source_folder=source_folder_file_path, criteria=criteria)
 
-    sorted_files.sort()
     record_files.sort()
-    if(sorted_files == record_files) or (len(record_files) == 0):
+    if(len(record_files) == 0):
         print('No new files')
     else:
         # Some sort of function to sort through the new files
         for i in range(len(record_files)):
             clean_file(record_files[i])
-##        for item in record_files:
-##            clean_file(item) 
         
 
 if __name__ == '__main__':
