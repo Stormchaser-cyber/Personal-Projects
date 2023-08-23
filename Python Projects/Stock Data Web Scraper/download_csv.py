4a640edf2e6c0cd4fd89a8e3448c8ec063ac3551
Stormchaser-cyber <ted.strombeck1@gmail.com>
2023-08-23 10:22:35 -0500

    Added beta functionality

--- Python Projects/Stock Data Web Scraper/download_csv.py
+++ Python Projects/Stock Data Web Scraper/download_csv.py
@@ -2,7 +2,7 @@
 #
 # Created -- Ted Strombeck -- July 2021 
 # Last Updated -- August 23, 2023
-# Version 1.0.4
+# Version 1.0.5
 # 
 
 # URL: "https://www.nasdaq.com/market-activity/stocks/screener"
@@ -588,6 +588,70 @@ def download_fcf_margin_percentage_for_stock_ticker(stock_ticker):
         last_time_downloaded = datetime.datetime.now()
 
     #return fcf margin % stats
+    return "{:s},{:s}".format(current_value,last_updated)
+
+def download_beta_for_stock_ticker(stock_ticker):
+    """
+    
+    """
+    results = []
+    current_value = 0.00
+    last_updated = "n/a"
+    result_path = '/html/body/div[2]/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div[1]/strong/text()[1]'
+    url = "https://www.google.com/"
+
+    if (len(stock_ticker) == 0):
+        raise selenium_exceptions.InvalidArgumentException
+    else:
+        url = "https://www.gurufocus.com/term/beta/A/Beta/Agilent%20Technologies"
+
+    ### Works! Just takes a little bit of awhile, not too sure why ###
+    options = webdriver.ChromeOptions()
+
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    
+    #options.add_argument('--headless') # Not showing the webpage pop up
+    
+    options.add_argument('--log-level=3') # Only showing logs if they are fatal
+    
+    browser = webdriver.Chrome(options=options) # Creating a chrome browser object
+
+    browser.get(url) # looking up the URL of the website
+
+    #ActionChains(browser).scroll_by_amount(0, 1000).perform()
+    browser.maximize_window()
+
+    time.sleep(10) # sleeping for 10 seconds once we get on the webpage so we have time for everything to load
+
+    try:
+        browser.find_element(By.ID, "cboxClose").click()
+        time.sleep(2)
+        search_ticker = browser.find_element(By.ID, "symbol")
+        search_ticker.send_keys(stock_ticker)
+        time.sleep(2)
+        browser.find_element(By.ID, "def_page_ticker_switch").click()
+        time.sleep(5)
+        results = browser.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div/div[2]/font[1]").get_attribute("innerHTML")
+        
+        results=results.replace(":", "")
+        results=results.strip()
+        results = results.split()
+
+        current_value = results[0]
+        last_updated = " ".join(results[1:len(results)])
+        
+
+    except Exception as ex:
+        print('Unsupported Exception caught: ',end='')
+        print(type(ex))
+        print(ex)
+        browser.quit()
+    finally:
+        browser.quit() # closing all terminals and windows
+
+        last_time_downloaded = datetime.datetime.now()
+
+    #return beta stats
     return "{:s},{:s}".format(current_value,last_updated)
 
 def download_csv_from_url_by_xpath(url, xpath):
