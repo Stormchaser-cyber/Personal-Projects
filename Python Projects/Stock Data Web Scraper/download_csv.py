08c9a37d0aaf6a3a8f4f394b8acf4611db894e0f
Stormchaser-cyber <ted.strombeck1@gmail.com>
2023-08-23 08:16:53 -0500

    Added debt-to-equity functionality

--- Python Projects/Stock Data Web Scraper/download_csv.py
+++ Python Projects/Stock Data Web Scraper/download_csv.py
@@ -154,7 +154,6 @@ def download_pb_for_stock_ticker(stock_ticker):
     #return pb stats
     return "{:s},{:s},{:s},{:s}".format(current_value,min_value,med_value,max_value)
 
-
 def download_dividend_yield_for_stock_ticker(stock_ticker):
     """
     download_pb_for_stock_ticker downloads the related pb for a selected stock
@@ -295,6 +294,83 @@ def download_yoy_ebitda_growth_rate_for_stock_ticker(stock_ticker):
 
     #return Dividend yield stats
     return "{:s},{:s}".format(current_value,last_updated)
+
+def download_debt_to_equity_for_stock_ticker(stock_ticker):
+    """
+    
+    """
+    results = []
+    current_value = 0.00
+    min_value = 0.00
+    med_value = 0.00
+    max_value = 0.00
+    result_path = '/html/body/div[2]/div[2]/div/div/div/div[2]/div[2]/div[1]/div/div[1]/strong/text()[1]'
+    url = "https://www.google.com/"
+
+    if (len(stock_ticker) == 0):
+        raise selenium_exceptions.InvalidArgumentException
+    else:
+        url = "https://www.gurufocus.com/term/deb2equity/A/Debt-to-Equity/Agilent%20Technologies"
+
+    ### Works! Just takes a little bit of awhile, not too sure why ###
+    options = webdriver.ChromeOptions()
+
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    
+    #options.add_argument('--headless') # Not showing the webpage pop up
+    
+    options.add_argument('--log-level=3') # Only showing logs if they are fatal
+    
+    browser = webdriver.Chrome(options=options) # Creating a chrome browser object
+
+    browser.get(url) # looking up the URL of the website
+
+    #ActionChains(browser).scroll_by_amount(0, 1000).perform()
+    browser.maximize_window()
+
+    time.sleep(10) # sleeping for 10 seconds once we get on the webpage so we have time for everything to load
+
+    try:
+        browser.find_element(By.ID, "cboxClose").click()
+        time.sleep(2)
+        search_ticker = browser.find_element(By.ID, "symbol")
+        search_ticker.send_keys(stock_ticker)
+        time.sleep(2)
+        browser.find_element(By.ID, "def_page_ticker_switch").click()
+        time.sleep(5)
+        results = browser.find_element(By.ID, "deb2equity_tools").get_attribute("outerHTML").split(">")
+
+        for item in results:
+            if "Min" in item:
+                values = item
+            elif "Current" in item:
+                current = item
+            # can expand to percentage if desired/needed
+        
+
+        values=values.replace("&nbsp;&nbsp;", "")
+        values=values.replace("\n\n", "")
+        values=values.split(" ")
+
+        min_value = values[1]
+        med_value = values[3]
+        max_value = values[5]
+
+        current=current.replace("Current:", "")
+        current_value=current.replace("</strong", "").strip()
+
+    except Exception as ex:
+        print('Unsupported Exception caught: ',end='')
+        print(type(ex))
+        print(ex)
+        browser.quit()
+    finally:
+        browser.quit() # closing all terminals and windows
+
+        last_time_downloaded = datetime.datetime.now()
+
+    #return debt-to-equity stats
+    return "{:s},{:s},{:s},{:s}".format(current_value,min_value,med_value,max_value)
 
 def download_csv_from_url_by_xpath(url, xpath):
     """
